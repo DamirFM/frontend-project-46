d25b23165a2d147a785bb166fb3339730225a7b4
Damir Yakupov <yakupovdamir93@gmail.com>
2023-03-13 23:44:46 -0400

    add

--- src/prog.js
+++ src/prog.js
@@ -1,3 +1,19 @@
 import genDiff from 'bin/genDiff';
 const result = genDiff(pathToFile1, pathToFile2);
 console.log(result);
+
+import fs from 'fs';
+import path from 'path';
+
+const readFile = (path) => {
+  const fullPath = path.resolve(path, process.cwd()); 
+  const data = fs.readFileSync(fullPath).toString();
+  return data;
+}
+
+export default (path1, path2) => {
+  const data1 = readFile(path1);
+  const data2 = readFile(path2);
+
+  // остальной код
+};
