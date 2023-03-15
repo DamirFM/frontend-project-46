42e6318edb8d4c46b5351995495e36a283e1793e
Damir Yakupov <yakupovdamir93@gmail.com>
2023-03-14 22:08:38 -0400

    add

--- src/prog.js
+++ src/prog.js
@@ -14,6 +14,23 @@ const readFile = (path) => {
 export default (path1, path2) => {
   const data1 = readFile(path1);
   const data2 = readFile(path2);
-
+  const current = {};
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const uni = _.union(keys1, keys2);
+  for (const key of uni) {
+    const check1 = Object.hasOwn(data1, key);
+    const check2 = Object.hasOwn(data2, key);
+    if (!check2) {
+      current[key] = 'deleted';
+    } else if (check2) {
+      current[key] = 'added';
+    } if (check1 && check2) {
+      current[key] = 'changed';
+    } if (data1[key] === data2[key]) {
+      current[key] = 'unchanged';
+    }
+  }
+  return current;
   // остальной код
 };
