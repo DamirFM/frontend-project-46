6e6192ca1c32eb3bf22708a24a063e4aa015cfe3
Damir Yakupov <yakupovdamir93@gmail.com>
2023-03-21 00:07:42 -0400

    add index update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
+import {readFileSync} from 'fs';
 
-import _ from 'lodash';
-const obj1 = path.resolve('../__fixtures__/file1.json');
-const obj2 = path.resolve('../__fixtures__/file2.json');
+export default function (filepath1, filepath2)  {
 
-const firstObj = JSON.parse(obj1);
-const secondObj = JSON.parse(obj2);
-// BEGIN (write your solution here)
-export default function genDiff(firstObj, secondObj) {
-    const current = {};
-    const keys1 = Object.keys(firstObj);
-    const keys2 = Object.keys(secondObj);
-    const uni = _.union(keys1, keys2);
-    for (const key of uni) {
-      const check1 = Object.hasOwn(firstObj, key);
-      const check2 = Object.hasOwn(secondObj, key);
-      if (!check2) {
-        current[key] = 'deleted';
-      } else if (check2) {
-        current[key] = 'added';
-      } if (check1 && check2) {
-        current[key] = 'changed';
-      } if (firstObj[key] === secondObj[key]) {
-        current[key] = 'unchanged';
-      }
+const data1 = readFileSync(filepath1, 'utf-8');
+const data2 = readFileSync(filepath2, 'utf-8');
+const parsedData1 = JSON.parse(data1);
+const parsedData2 = JSON.parse(data2);
+
+console.log(parsedData1)
+console.log(parsedData2)
+}  
+
+function compare (path1, path2)  {
+  const data1 = readFile(path1);
+  const data2 = readFile(path2);
+  const current = {};
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const uni = _.union(keys1, keys2);
+  for (const key of uni) {
+    const check1 = Object.hasOwn(data1, key);
+    const check2 = Object.hasOwn(data2, key);
+    if (!check2) {
+      current[key] = 'deleted';
+    } else if (check2) {
+      current[key] = 'added';
+    } if (check1 && check2) {
+      current[key] = 'changed';
+    } if (data1[key] === data2[key]) {
+      current[key] = 'unchanged';
     }
-    return current;
   }
-  
-  // END
+  return current;
+  // остальной код
+};
