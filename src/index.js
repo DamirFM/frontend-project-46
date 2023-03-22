a15bb714157dd6ece56d5da2b064b9d0e4955db9
Damir Yakupov <yakupovdamir93@gmail.com>
2023-03-21 23:24:44 -0400

    add index.js update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import {readFileSync} from 'fs';
+import  _  from 'lodash';
 
-export default function (filepath1, filepath2)  {
-
-const data1 = readFileSync(filepath1, 'utf-8');
-const data2 = readFileSync(filepath2, 'utf-8');
-const parsedData1 = JSON.parse(data1);
-const parsedData2 = JSON.parse(data2);
-
-console.log(parsedData1)
-console.log(parsedData2)
-}  
-
-function compare (path1, path2)  {
+function denDiff (path1, path2)  {
   const data1 = readFile(path1);
   const data2 = readFile(path2);
   const current = {};
@@ -19,18 +9,33 @@ function compare (path1, path2)  {
   const keys2 = Object.keys(data2);
   const uni = _.union(keys1, keys2);
   for (const key of uni) {
-    const check1 = Object.hasOwn(data1, key);
-    const check2 = Object.hasOwn(data2, key);
-    if (!check2) {
-      current[key] = 'deleted';
-    } else if (check2) {
-      current[key] = 'added';
-    } if (check1 && check2) {
-      current[key] = 'changed';
-    } if (data1[key] === data2[key]) {
-      current[key] = 'unchanged';
-    }
+    console.log(key)
+    // const check1 = Object.hasOwn(data1, key);
+    // const check2 = Object.hasOwn(data2, key);
+    // if (!check2) {
+    //   current[key] = 'deleted';
+    // } else if (check2) {
+    //   current[key] = 'added';
+    // } if (check1 && check2) {
+    //   current[key] = 'changed';
+    // } if (data1[key] === data2[key]) {
+    //   current[key] = 'unchanged';
+    // }
   }
   return current;
-  // остальной код
 };
+
+export default function (filepath1, filepath2)  {
+
+
+  
+const data1 = readFileSync(filepath1, 'utf-8');
+const data2 = readFileSync(filepath2, 'utf-8');
+const parsedData1 = JSON.parse(data1);
+const parsedData2 = JSON.parse(data2);
+
+console.log(parsedData1)
+console.log(parsedData2)
+denDiff(parsedData1,parsedData2);
+}  
+
