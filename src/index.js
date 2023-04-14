837b32d39a1050f6421b181addc93980e3c2f741
Damir Yakupov <yakupovdamir93@gmail.com>
2023-04-13 23:31:46 -0400

    add new lines

--- src/index.js
+++ src/index.js
@@ -2,18 +2,32 @@ import fs from 'fs';
 import  _  from 'lodash';
 
 const genDiff = (data1, data2, replacer =' ') => {
-  // const keys1 = Object.keys(data1);
-  // const keys2 = Object.keys(data2);
-  // const keys = _.union(keys1, keys2); 
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.union(keys1, keys2); 
+  const sortedKeys = _.sortBy(keys);
 
   const indentSize = 1; // размер пробела
   const currentIndent = replacer.repeat(indentSize); //формирование отступа для текущей строки
   const brecketIndent = replacer.repeat(indentSize - 1);
 
-  const mergedObjects = Object.assign(data1, data2);
-  const array = Object.entries(mergedObjects);
-  //console.log(mergedObjects[value])
-  const lines = array.map(([key, val]) => `${currentIndent}${key}:  ${val}`);
+
+  const lines = sortedKeys.map((key) => {
+    if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+      return `${currentIndent}+${currentIndent}${key}` //added
+
+    } else if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
+      return `${currentIndent}-${currentIndent}${key}` //deleted
+
+    } else if (data1[key] === data2[key]) {
+      return `${currentIndent}${currentIndent}${key}`  //unchanged
+
+    // } else {
+    //   result[key] = 'unchanged';
+     }
+  
+  });
+
   const result = [`{`, ...lines, `${brecketIndent}}`].join('\n');
 
   
