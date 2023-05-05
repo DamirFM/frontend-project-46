aaaa2d3a6a933ea841eec1d79d5ee1065ba5a9fd
Damir Yakupov <yakupovdamir93@gmail.com>
2023-05-05 00:39:10 -0400

    add __tests__

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
-import { describe, expect, test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 // eslint-disable-next-line import/no-extraneous-dependencies
 // import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
@@ -10,12 +10,6 @@ const filename = fileURLToPath(import.meta.url);
 const dirName = dirname(filename);
 const readFile = (nameOfFile) => readFileSync(resolve(dirName, '..', '__fixtures__', nameOfFile), 'utf-8');
 
-const cases = [
-  ['__fixtures__/file1.json', '__fixtures__/file2.json', readFile('jsonTest.txt'), 'json'],
-];
-
-describe('output format', () => {
-  test.each(cases)('difference %s and %s', (a, b, result, format = 'stylish') => {
-    expect(genDiff(a, b, format)).toEqual(result);
-  });
+test('TEST', () => {
+  expect(genDiff(JSON.parse(readFile('file1.json')), JSON.parse(readFile('file2.json')))).toEqual(readFile('jsonTest.txt'));
 });
