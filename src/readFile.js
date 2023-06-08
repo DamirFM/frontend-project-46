c142c49afcec7b909c1fb405accffbf09d6aa3b4
Damir Yakupov <yakupovdamir93@gmail.com>
2023-06-07 23:49:20 -0400

    add

--- src/readFile.js
+++ src/readFile.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 // eslint-disable-next-line import/named
 import { genDiff } from './index.js';
 
+
 export default (filepath1, filepath2) => {
   const data1 = fs.readFileSync(filepath1, 'utf-8');
   const data2 = fs.readFileSync(filepath2, 'utf-8');
